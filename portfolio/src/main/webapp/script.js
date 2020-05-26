4cbecee40816367d77c87e2d6c55c340c5c9b0ee
Chris <christopher.desir01@gmail.com>
2020-05-26 18:02:44 -0400

    Removes styling components

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,25 +13,6 @@
 // limitations under the License.
 
 /**
- * Automatically plays the slideshow gallary!
- */
-var myIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
-}
-
-
-/**
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
