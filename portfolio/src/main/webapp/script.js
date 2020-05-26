b7bfff10ec665b5cc54a005f414096f69d1a6c3d
Chris <christopher.desir01@gmail.com>
2020-05-26 15:03:11 -0400

    Adds a section for a gallery, prepares project page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,6 +13,28 @@
 // limitations under the License.
 
 /**
+ * Allows the user to manually click through a slideshow gallary!
+ */
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+
+/**
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
