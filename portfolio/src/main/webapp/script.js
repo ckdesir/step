a6fa26e0313db111343be3384930f94526f8ebb4
Chris <christopher.desir01@gmail.com>
2020-05-28 17:57:31 -0400

    Adds the slideshow to both the gallery and to the blog

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -33,12 +33,12 @@ function showSpecificSlide(n) {
 
 function carousel() {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName("blog-slides");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  setTimeout(carousel, 4000); 
 }
