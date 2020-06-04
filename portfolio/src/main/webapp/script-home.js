1fee766b8fb2948edd028e1064ab8828dd4bbd72
Chris <christopher.desir01@gmail.com>
2020-06-04 18:22:41 +0000

    Style change

--- portfolio/src/main/webapp/script-home.js
+++ portfolio/src/main/webapp/script-home.js
@@ -48,4 +48,9 @@ function main() {
         function adjustForwardOne() {
           slideShowGallery.adjustSlideManual(ADJUST_FORWARD); 
     }
+    // The button is replaced with he response from the servlet, Who Am I?
+    fetch('/data').then((response) => response.text()).then((introHeading) => {
+        document.getElementById('intro-heading').innerText = introHeading;
+        document.getElementById('heading-button').style.display = 'none';
+    });
 }
